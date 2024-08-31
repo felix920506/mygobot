9cb1471bfedc1e1d1e84608a9257251cabf28790
felix920506 <felix920506@gmail.com>
2024-08-31 10:42:32 -0400

    image mapping helper

---
+++ image_map.py
@@ -0,0 +1,20 @@
+import json
+
+with open('image_map.json', 'r', encoding='utf8') as mapfile:
+    raw_mappings = json.load(mapfile)
+
+mappings = {}
+
+for i in raw_mappings:
+    mappings[i['name']] = i['filename']
+
+def get_filename(name: str) -> str | None:
+    if name in mappings:
+        return mappings['name']
+    return None
+
+def get_all_names() -> list:
+    return mappings.keys()
+
+def get_all_files() -> list:
+    return mappings.values()
