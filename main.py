d826aec2c25445b94bb35e6aec84265953fed0a4
felix920506 <felix920506@gmail.com>
2024-08-31 10:32:50 -0400

    write something in main and config

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import yaml
+import json
+import os
+
+with open('config.yml', 'r', encoding='utf8') as configfile:
+    SETTINGS = yaml.load(configfile, yaml.Loader)
+
+# deal with settings
+if SETTINGS['send-as-attachment'] & SETTINGS['download-files']:
+    if not os.path.isdir('./img'):
+        try:
+            os.mkdir('./img')
+        except:
+            print('failed to make storage dir, quitting')
+            exit()
+    
+    if SETTINGS['download-at-startup']:
+        pass
